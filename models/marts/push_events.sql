48b3c90d9cec9103a21553abc5659401e9815bb4
winnie <91998347+gwenwindflower@users.noreply.github.com>
2023-09-24 11:14:31 -0500

    Sort out differing db names (#8)
    early on i was calling the db github_archive and then later octocatalog and it made much confuse

---
+++ models/marts/push_events.sql
@@ -0,0 +1,30 @@
+with
+
+push_events as (
+
+    select * from {{ ref('stg_events') }}
+
+    where event_type in ('PushEvent')
+
+),
+
+unnest_json as (
+
+    select
+        event_id,
+        actor_id,
+        payload,
+        repo_id,
+        repo_name,
+        actor_login,
+        event_created_at,
+        payload ->> '$.push_id' as push_id,
+        payload ->> '$.size' as push_size,
+        payload ->> '$.distinct_size' as push_distinct_size,
+        payload -> '$.commits' as push_commits,
+
+    from push_events
+
+)
+
+select * from unnest_json
